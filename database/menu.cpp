80671b718a0c5f46dccaf76f26aacd89330474d2
Insight_GX <gongxuncd@gmail.com>
2018-06-04 15:39:36 +0000

    menu.cpp created online with Bitbucket

--- database/menu.cpp
+++ database/menu.cpp
@@ -1,7 +1,7 @@
 #include "database.hpp"
 #include <ctime>
-#include <cstdlib>
-#include <map>
+#include <fstream>
+#include <iostream>
 
 using db = database<int, long long>;
 using iter = database<int, long long>::iterator;
@@ -16,22 +16,44 @@ int main() {
 	const char a[] = "idxfile";
 	const char b[] = "datafile";
 	db mydb(a, b);
-	mydb.insert(1,1);
-	cout << mydb.empty();
+
+	ofstream out("testfile");
+	out.close();
+	fstream file("testfile");
+
+	for (size_t i = 0; i < 200; ++i) {
+		k = rand() % 200;
+		mydb.insert(k, i);
+		cout << k << ' ' << i << '\n';
+		file << k << ' ' << i << '\n';
+	}
+	file << endl;
+	for (size_t i = 0; i < 200; ++i) {
+		k = rand() % 200;
+		mydb.erase(k);
+		cout << k << ' ' << i << '\n';
+		file << k << ' ' << i << '\n';
+	}
+	file << endl;
+	for (size_t i = 0; i < 200; ++i) {
+		k = rand() % 200;
+		mydb.insert(i, k);
+		cout << k << ' ' << i << '\n';
+		file << k << ' ' << i << '\n';
+	}
+	file << endl;
+	for (size_t i = 0; i < 100; ++i) {
+		mydb.modify(i, i);
+		cout << i << ' ' << i << '\n';
+		file << i << ' ' << i << '\n';
+	}
+	file << endl;
+	for (size_t i = 0; i < 100; ++i) {
+		iter tmp = mydb.find(i);
+		cout << i << " find  " << *tmp << '\n';
+		file << i << ' ' << i << '\n';
+	}
+	file << endl;
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
