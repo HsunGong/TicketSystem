7e7f5040150841e50f2cef1c11e40f253e1d24c9
Insight_GX <gongxuncd@gmail.com>
2018-06-06 14:35:16 +0000

    menu.cpp created online with Bitbucket

--- database/menu.cpp
+++ database/menu.cpp
@@ -11,7 +11,7 @@ struct cmp {
 	template<class Key>
 	bool operator ()(const Key &a, const Key &b)const { return a.x > b.x; }
 };
-#define mysize 5000
+#define mysize 500
 int p[mysize];
 void swap(int &a, int&b) {
 	int c;
@@ -20,7 +20,7 @@ void swap(int &a, int&b) {
 	b = c;
 }
 void init() {
-	size_t i;
+	int32_t i;
 	for (i = 0; i < mysize; ++i) p[i] = i;
 	for (i = mysize - 1; i > 0; --i) swap(p[i], p[rand() % i]);
 
@@ -29,7 +29,7 @@ void init() {
 int main() {
 	//srand(time(0));
 	init();
-	size_t i, j, k;
+	int32_t i, j, k;
 	const char a[] = "idxfile";
 	const char b[] = "datafile";
 	db mydb(a, b);
@@ -40,60 +40,71 @@ int main() {
 	iter tmp = mydb.begin();
 	map<int, long long>::iterator it = m.begin();
 
-	for (size_t i = 0; i < mysize; ++i) {
+	for (int32_t i = 0; i < mysize; ++i) {
 		k = p[i];
 		//k = 100 - i;
 		mydb.insert(k, i);		
 		m.insert(pair<int, long long>(k, i));
+
 		tmp = mydb.find(k);
 		it = m.find(k);
 		//cout << *tmp << " and " << it->second << '\n';
+		if (*tmp != it->second) {
+			cout << i << " find  " << *tmp;
+			cout << " map " << it->second << '\n';
+		}
 	}
-	//cout << endl;
+	for (int32_t i = 0; i < mysize; ++i) {
+		k = p[i];
+		tmp = mydb.find(k);
+		it = m.find(k);
+		//cout << *tmp << " and " << it->second << '\n';
+		if (*tmp != it->second) {
+			cout << i << " find  " << *tmp;
+			cout << " map " << it->second << '\n';
+		}
+	}
+
+
+	for (int32_t i = 0; i < 300; ++i) {
+		k = p[i];
+		mydb.modify(k, k);
+		m[k] = k;
+		tmp = mydb.find(k);
+		it = m.find(k);
+		//cout << *tmp << " and " << it->second << '\n';
+		if (*tmp != it->second) {
+			cout << i << " find  " << *tmp;
+			cout << " map " << it->second << '\n';
+		}
+	}cout << endl;
 	//tmp = mydb.begin();
 	//it = m.begin();
-	//for (size_t i = 0; i < mysize; ++i) {
+	//for (int32_t i = 0; i < mysize; ++i) {
 	//	if (*tmp != it->second) {
 	//		cout << i << " find  " << *tmp;
 	//		cout << " map " << it->second << '\n';
 	//	}
-
 	//	++tmp;
 	//	++it;
 	//}cout << endl;
 
-
-	//for (size_t i = 0; i < 300; ++i) {
-	//	mydb.modify(i, i);
-	//	m[i] = i;
-	//}cout << endl;
-	//tmp = mydb.begin();
-	//it = m.begin();
-	//for (size_t i = 0; i < mysize; ++i) {
-	//	if (*tmp != it->second) {
-	//		cout << i << " find  " << *tmp;
-	//		cout << " map " << it->second << '\n';
-	//	}
-	//	++tmp;
-	//	++it;
-	//}cout << endl;
-
-	int er = 50;
-	for (size_t i = 0; i < er; ++i) {
+	int er = mysize;
+	for (int32_t i = 0; i < er; ++i) {
 		k = p[i];
 		mydb.erase(k);
 		m.erase(k);
 	}cout << endl;
 
-	tmp = mydb.begin();
-	it = m.begin();
-	for (size_t i = 0; i < mysize - er; ++i) {
+	for (int32_t i = er; i < mysize; ++i) {
+		k = p[i];
+		tmp = mydb.find(k);
+		it = m.find(k);
+		//cout << *tmp << " and " << it->second << '\n';
 		if (*tmp != it->second) {
 			cout << i << " find  " << *tmp;
 			cout << " map " << it->second << '\n';
 		}
-		++tmp;
-		++it;
 	}cout << endl;
 	return 0;
 }
